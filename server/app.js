94c5458a170ca31f18c6f3fcd9fbe4fd02e834c5
Denzel Fuentes <denzel.fuentes.n@gmail.com>
2023-04-02 16:02:42 -0400

    Agre

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ import express from "express";
 import postRoutes from './routes/post.routes.js'
 
 const app = express()
+
 //configuracion de ejs
 app.set("view engine","ejs")
+
 //middleware
 app.use(express.json());
+
+//Servir el archivo principal
+app.use('/',express.static('./public'))
+
 //usar las rutas que importamos
 app.use(postRoutes);
 
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,13 +1,18 @@
-
 // Aqui declararemos las rutas  y los metodos GET POS PUT ....
 import { Router } from "express";
 const router = Router();
 
+router.get('/login',(req,res)=>{
+    res.render('login');
+});
 
 
-router.get('/',(req,res)=>{
-    res.render('index')
+router.get('/createUser',(req,res)=>{
+    res.render('CreateUser');
+});
+
+router.get('Administrador',(req,res)=>{
+    res.render('index');
 })
-
 
 export default router;
